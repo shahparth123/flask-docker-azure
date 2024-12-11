34a9b7add7c85f3b7d0ff70b53d93f51f4445061
Parth Shah <parth@parthhosting.com>
2024-12-11 08:42:11 +0530

    API creation

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask import jsonify
 
 app = Flask(__name__)
 
@@ -26,6 +27,18 @@ def submit():
         name = request.form['name']
         return f"Hello, {name}!"
     return render_template('form.html')
+
+@app.route('/api/users', methods=['GET'])
+def users():
+    users= [{
+        "user_id":1,
+        "username":"parth"
+    },{
+    "user_id":2,
+    "username":"ajit"
+    }
+    ]
+    return jsonify(users)
 
 if __name__ == '__main__':
     app.run(debug=True,host="0.0.0.0",port=5000)
