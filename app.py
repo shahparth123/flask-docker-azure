552e174de1be385f1065d0e8d9e15fb1e9aa4de0
Parth Shah <parth@parthhosting.com>
2024-12-11 08:32:48 +0530

    custom routing

--- app.py
+++ app.py
@@ -6,5 +6,14 @@ app = Flask(__name__)
 def home():
     return "Hello, Flask!"
 
+
+@app.route('/hello/<username>')
+def hello_user(username):
+    return f"Hello, {username}"
+
+@app.route('/hello-user-id/<int:user_id>')
+def hello_user_id(user_id):
+    return f"User ID: {user_id}"
+
 if __name__ == '__main__':
     app.run(debug=True,host="0.0.0.0",port=5000)
